137917c58b5ee551a50d4362488ff526fe2082ea
ShirleyYing <52013975+ShirleyYingok@users.noreply.github.com>
2019-09-19 17:32:32 -0400

    Create front_times.py

---
+++ warmup-2/front_times.py
@@ -0,0 +1,16 @@
+def front_times(str, n):
+
+    '''
+    Given a string and a non-negative int n, 
+    we'll say that the front of the string is the first 3 chars, 
+    or whatever is there if the string is less than length 3. 
+    Return n copies of the front
+    '''
+    
+    # starting from 0 stop before 3
+    return str[0:3] * n
+
+print(front_times('Chocolate', 2))
+print(front_times('Chocolate', 3))
+print(front_times('Abc', 3))
+
